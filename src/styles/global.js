58f5a399f14dabc9d4d7d8f3987c1cd0a8ad8fb5
Douglas Brandão <douglasfrb@gmail.com>
2020-02-07 16:37:07 -0400

    Estruturaçao de pastas do projeto

---
+++ src/styles/global.js
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from "styled-components";
+
+const EstiloGlobal = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+  }
+
+  body {
+    background: #FFF;
+    font-family: sans-serif;
+  }
+`;
+
+export default EstiloGlobal;
